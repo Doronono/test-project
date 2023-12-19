1fcb0a332ae7e9f2dda6fd4f4f06b47f62bc6933
Doronono <doronono.ai@gmail.com>
2023-12-19 17:28:56 +0800

    Assets

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    /*
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireForce = 20f;
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+        }
+        
+
+
+    }*/
+}
---
+++ Assets/Scripts/WeaponRenderer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WeaponRenderer : MonoBehaviour
+{
+    [SerializeField]
+    protected int playerSortingOrder = 0;
+    protected SpriteRenderer weaponRenderer;
+
+    private void Awake()
+    {
+        weaponRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void FlipSprite(bool val)
+    {
+        /*weaponRenderer.flipY = val;*/ //For Gun
+        weaponRenderer.flipX = val;
+    }
+
+    public void RenderBehindHead(bool val)
+    {
+        if (val)
+            weaponRenderer.sortingOrder = playerSortingOrder - 1;
+        else
+            weaponRenderer.sortingOrder = playerSortingOrder + 1;
+    }
+
+}
